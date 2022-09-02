851da74fdbe47b5dcfa29d1b88867823ffd6bb89
Ashish Singh <ashish1qwerty1@gmail.com>
2022-09-02 18:43:45 +0530

    Add sample2.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
 print("hello world")
 for n in range(10):
-    print("{} ^ 2 = {}.".format(n, lambda n: n**2))
+    print("{} ^ 2 = {}.".format(n, n**2))
 print("Done")
---
+++ sample2.py
@@ -0,0 +1,7 @@
+print("hello world")
+def square(n):
+    return n**2
+for i in range(10):
+    print("{} ^ 2 = {}.".format(i, square(i)))
+print("Done")
+print("BYE")
